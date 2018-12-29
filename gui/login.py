c5dffbf738e865c90b4da8a4c7eb350beea80cb9
Wind <ice.jackey@gmail.com>
2018-12-29 16:51:02 +0800

    login init

--- gui/login.py
+++ gui/login.py
@@ -7,7 +7,9 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QWidget, QLineEdit
+from PyQt5.QtCore import QRegExp
+from PyQt5.QtGui import QRegExpValidator
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -21,18 +23,28 @@ class Ui_MainWindow(object):
         font.setPointSize(16)
         self.textEdit.setFont(font)
         self.textEdit.setObjectName("textEdit")
-        self.textEdit_2 = QtWidgets.QTextEdit(self.centralwidget)
+        # password
+        self.textEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
         self.textEdit_2.setGeometry(QtCore.QRect(130, 150, 281, 51))
         font = QtGui.QFont()
         font.setPointSize(16)
         self.textEdit_2.setFont(font)
         self.textEdit_2.setObjectName("textEdit_2")
+        self.textEdit_2.setEchoMode(QLineEdit.Password)
+        regx = QRegExp("^[a-zA-Z][0-9A-Za-z]{14}$")  # 为给定的模式字符串构造一个正则表达式对象。
+        # 构造一个验证器，该父对象接受与正则表达式匹配的所有字符串。这里的父对象就是QLineEdit对象了。匹配是针对整个字符串; 例如：如果正则表达式是[A-Fa-f0-9]+将被视为^[A-Fa-f0-9]+$。
+        validator = QRegExpValidator(regx, self.textEdit_2)
+        # 将密码输入框设置为仅接受符合验证器条件的输入。 这允许您对可能输入的文本设置任何约束条件。因此我们这里设置的就是符合上面描述的三种约束条件。
+        self.textEdit_2.setValidator(validator)
+
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(70, 260, 111, 41))
         self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.login)
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_2.setGeometry(QtCore.QRect(260, 260, 111, 41))
         self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.clicked.connect(self.register)
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(30, 60, 91, 41))
         font = QtGui.QFont()
@@ -62,6 +74,19 @@ class Ui_MainWindow(object):
         self.pushButton_2.setText(_translate("MainWindow", "注册"))
         self.label.setText(_translate("MainWindow", "用户名"))
         self.label_2.setText(_translate("MainWindow", "密码"))
+
+    def login(self):
+        name = self.textEdit.toPlainText()
+        password = self.textEdit_2.text()
+        print("[DEBUG]Login with name %s and password %s"%(name, password))
+        ''''''
+        #登陆处理
+        ''''''
+        pass
+
+    def register(self):
+        print("Register")
+        pass
 
 class window(QWidget, Ui_MainWindow):
     def __init__(self):
