efd02bb6133197f7f45f4b22aaba1bd738a89ea3
Wind <ice.jackey@gmail.com>
2019-01-04 22:53:25 +0800

    show meta

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -14,10 +14,12 @@ import requests
 import json
 
 class MainWindow(QtWidgets.QWidget):
-
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setObjectName("Main")
+        self.filelist = []
+        root_dir = "/user/root"
+
         self.resize(1024, 768)
         self.centralwidget = QtWidgets.QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
@@ -31,25 +33,20 @@ class MainWindow(QtWidgets.QWidget):
         self.dirEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.dirEdit.setGeometry(QtCore.QRect(20, 10, 881, 31))
         self.dirEdit.setObjectName("textEdit")
-        self.dirEdit.setText("/user/root")
+        self.current_dir = root_dir
+        self.dirEdit.setText(root_dir)
 
         # 文件列表
         self.listView = QtWidgets.QListWidget(self.centralwidget)
         self.listView.setGeometry(QtCore.QRect(20, 60, 761, 681))
         self.listView.setObjectName("listView")
-
-        # load test
-        self.listView.addItem("file_1")
-        self.listView.addItem("file_2")
-        self.listView.addItem("file_3")
-        #self.listView.doubleClicked(i).
-        # load test
+        self.listView.doubleClicked.connect(self.item_doubleclick)
 
         # 文件属性
-        self.textEdit_2 = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit_2.setGeometry(QtCore.QRect(800, 60, 201, 681))
-        self.textEdit_2.setObjectName("textEdit_2")
-        self.textEdit_2.setEnabled(False) # 禁止选定，从而避免编辑
+        self.FileElement = QtWidgets.QTextEdit(self.centralwidget)
+        self.FileElement.setGeometry(QtCore.QRect(800, 60, 201, 681))
+        self.FileElement.setObjectName("textEdit_2")
+        self.FileElement.setEnabled(False) # 禁止选定，从而避免编辑
 
         self.retranslateUi(self)
         QtCore.QMetaObject.connectSlotsByName(self)
@@ -76,12 +73,60 @@ class MainWindow(QtWidgets.QWidget):
                      "dir_read_num": 999}}),
             'application/json'))
         ]
-        r = requests.post("http://172.18.95.74:8002/file", files=files)
+        try:
+            r = requests.post("http://172.18.95.74:8002/file", files=files)
+        except Exception as err:
+            print(format(err))
+            return
         contect = json.loads(r.content)
         if contect['status'] != 0:
             QMessageBox.warning(self, "错误", "无法打开该文件夹！", QMessageBox.Yes)
             return
+        self.current_dir = current_dir
         self.listView.clear()
+        self.filelist.clear()
         for _name in contect['data']['entries']:
             self.listView.addItem(_name)
+            self.filelist.append(_name)
 
+    def item_doubleclick(self, qtindex):
+        index = qtindex.row()
+        if (index >= len(self.filelist)):
+            return
+        selected_filename = self.filelist[index]
+        # 文件夹
+        if selected_filename[0] == '/':
+            current_dir = self.dirEdit.text()
+            current_dir += selected_filename[1:]
+            self.dirEdit.setText(current_dir)
+            self.get_list_from_dir()
+            return
+        # 文件
+        files = [
+            ('json', ("action", json.dumps({
+                "action": "read_meta",
+                "token": self.token,
+                "param":
+                    {"dir_root": self.current_dir,
+                     "file_name": selected_filename}}),
+                      'application/json'))
+        ]
+        try:
+            r = requests.post("http://172.18.95.74:8002/file", files=files)
+        except Exception as err:
+            print(format(err))
+            return
+        contect = json.loads(r.content)
+        if contect['status'] == 0:
+            text = ""
+            for data_key in contect['data']:
+                text += data_key
+                text += ": "
+                if type(contect['data'][data_key]) == type("string"):
+                    text += contect['data'][data_key]
+                else:
+                    text += str(contect['data'][data_key])
+                text += "\n"
+            self.FileElement.setText(text)
+        print(self.filelist[index])
+        pass
